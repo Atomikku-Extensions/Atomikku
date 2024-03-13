005e34a9a61b414e789776d44c01fcb530b35b5f
jhonatabezerra <jhonata.empresa+github@gmail.com>
2024-03-13 20:35:52 -0300

    Initial commit

---
+++ Atomikku/src/Atomikku.Core/Core.cs
@@ -0,0 +1,19 @@
+﻿namespace Atomikku.Core;
+
+public class Core
+{
+    public async Task GetSeachBook()
+    {
+        //var lightNovelToSearch = LightNovelToSearch();
+        //var searchBook = new Novelbin.Lib();
+    }
+
+    #region Private Methods
+
+    //private LightNovelToSearch LightNovelToSearch() => new()
+    //{
+    //    Tittle = "Tensei Shitara Slime Datta Ken"
+    //};
+
+    #endregion Private Methods
+}
